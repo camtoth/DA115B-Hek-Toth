1a93e87583c4bf5404707ba6de45e173f2016381
smallpotato <smallpotato6035@gmail.com>
2022-03-03 21:13:14 +0100

    feat: add main menu functionalities

---
+++ game/menu.py
@@ -0,0 +1,64 @@
+from re import sub
+import game
+
+
+class Menu:
+
+    def __init__(self):
+        self.new_game = game.Game()
+
+    def menuloop(self):
+        while(True):
+            # print main menu and take input
+            print("1 - Show rules\n2 - Start new game\n" +
+                "3 - Show leaderboard\n4 - Help\n5 - Exit")
+            selection = int(input("> "))
+            print(selection)
+
+            try:
+                if selection == 1:
+                    self.new_game.showRules()
+
+                elif selection == 2:
+                    self.submenuloop()
+                    
+                elif selection == 5:
+                    break
+
+            except BaseException:
+                pass
+
+    def submenuloop(self):
+        self.new_game.chooseRules()
+        self.new_game.choosePlayers()
+        self.new_game.output("1 - Change player name\n2 - Start game\n3 - Quit")
+
+        # 1 - Change player name
+        try:
+            subselection = int(input())
+            while(subselection == 1):
+                print("Input 1 to rename " +
+                      self.new_game.players[0].name +
+                      "or 2 to rename " +
+                      self.new_game.players[1].name)
+
+                try:
+                    player_ID = int(input())
+                    print("Enter new name: ")
+                    new_name = input()
+                    self.new_game.changeName(new_name, player_ID)
+                    self.new_game.output("1 - Change player name\n2 - Start game\n3 - Quit")
+                    subselection = int(input())
+                except BaseException:
+                    pass
+
+            # 2 - Start game
+            if subselection == 2:
+                self.new_game.runGame()
+
+            # 3 - Quit
+            elif subselection == 3:
+                exit()
+
+        except BaseException:
+            pass
